8de346d650c6f0ee19455a3ef35f7150628c624a
farkhanmaul <farkhanmaul@gmail.com>
2023-02-25 23:50:25 +0700

    first

---
+++ scripts.js
@@ -0,0 +1,13 @@
+// Sebuah fitur smooth scroll anchor points
+
+$('a[href*="#"]').on("click", function (e) {
+  e.preventDefault();
+
+  $("html, body").animate(
+    {
+      scrollTop: $($(this).attr("href")).offset().top,
+    },
+    500,
+    "linear"
+  );
+});
